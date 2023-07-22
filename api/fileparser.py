de392fd04c82c6182a2f438f6086cf55bb7cb728
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-22 17:48:20 -0600

    Creasted prompter.py

--- api/fileparser.py
+++ api/fileparser.py
@@ -1,10 +1,22 @@
 from pdfminer.high_level import extract_text
+from api.prompter import Subject
+from prompter import SpanishSubtopics
 from connection import ask
 
 prompt = """
-    The following file is a class study plan. Please provide some recommendations
-    for the teacher to improve their class study plan:
+    The following file is a class study plan. Please provide some recommendations considering the following information:
 """
+
+# add options that uses prompter.pyu
+
+subjet = input("What is the subject of the class study plan?")
+
+subtopic = input("What is the subtopic of the class study plan? ")
+time = input("How long does the class last? ")
+age = input("What is the age of the students? ")
+
+#map subject to subject enum
+
 text = extract_text("api/example.pdf")
 
 prompt += text
