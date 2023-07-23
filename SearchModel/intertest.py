0ee67c3ee3720e26b465bb34c567c44283f7641d
Oscar6647 <ocardenasg@yahoo.com>
2023-07-23 11:45:50 -0600

    new model

--- SearchModel/intertest.py
+++ SearchModel/intertest.py
@@ -1,3 +1,6 @@
+"""
+#Decision tree model
+
 import pickle
 import numpy as np
 with open ('Ometeotl.pkl', 'rb') as file:
@@ -6,6 +9,10 @@ test_data = np.array([[1, 1, 1, 1], [4,4,4,4]], dtype=int)
 predictions = model.predict(test_data)
 print("predicted classes: ")
 print(predictions)
+"""
+
+#checking for transalation pls ignore this
+
 """
 import pickle
 
@@ -25,3 +32,25 @@ else:
 """
 
 #neeeds dictionary to translate predicted classes and what the input translates too.
+
+#The following is code to test new Forest PICKLE VERSION
+
+import pickle
+import pandas as pd
+with open ('SearchModel\EduHack_RandomForest.pkl', 'rb') as file:
+    model = pickle.load(file)
+test_data = pd.DataFrame({"Categoría": [1], "Subcategoria": [3], "Edad de Estudiantes ": [9], "Tiempo de clase": [40]})
+predictions = model.predict(test_data)
+print("predicted classes: ")
+print(predictions)
+
+#JOBLIB VERSION
+
+import joblib
+import pandas as pd
+with open ('SearchModel\EduHack2.joblib', 'rb') as file:
+    model = joblib.load(file)
+test_data = pd.DataFrame({"Categoría": [1], "Subcategoria": [3], "Edad de Estudiantes ": [9], "Tiempo de clase": [40]})
+predictions = model.predict(test_data)
+print("predicted classes_joblib: ")
+print(predictions)
